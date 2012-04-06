a015a10ef9305bb2adffbd120ab869ea100260b1
SebastianFM <sebastiansobczyk@wp.pl>
2012-04-06 22:19:34 +0200

    GPU OC

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -773,7 +773,7 @@ static struct msm_bus_vectors grp3d_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(2484),
+		.ib = KGSL_CONVERT_TO_MBPS(2656),
 	},
 };
 
@@ -920,11 +920,6 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.io_fraction = 0,
 		},
 		{
-		        .gpu_freq = 266667000,
-			.bus_freq = 4,
-			.io_fraction = 0,
-		},
-		{
 			.gpu_freq = 228571000,
 			.bus_freq = 3,
 			.io_fraction = 33,
@@ -946,7 +941,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	},
 	.init_level = 1,
 	.max_level = 1,
-	.num_levels = 7,
+	.num_levels = 6,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/5,
 	.nap_allowed = true,
@@ -989,11 +984,16 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 		},
 		{
 			.gpu_freq = 200000000,
+			.bus_freq = 1,
+		},
+		{
+			.gpu_freq = 200000000,
 			.bus_freq = 0,
 		},
 	},
-	.init_level = 0,
-	.num_levels = 2,
+	.init_level = 1,
+	.max_level = 1,
+	.num_levels = 3,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
 	.nap_allowed = true,
@@ -1036,11 +1036,16 @@ static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 		},
 		{
 			.gpu_freq = 200000000,
+			.bus_freq = 1,
+		},
+		{
+			.gpu_freq = 200000000,
 			.bus_freq = 0,
 		},
 	},
-	.init_level = 0,
-	.num_levels = 2,
+	.init_level = 1,
+	.max_level = 1,
+	.num_levels = 3,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
 	.nap_allowed = true,
